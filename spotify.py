9e8adf2043eaf5fb69a6635833f53eeb5bfc711b
khabee123 <66386031+khabee123@users.noreply.github.com>
2020-07-14 21:36:31 +0700

    Add files via upload

---
+++ spotify.py
@@ -0,0 +1,57 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import time 
+
+client_id = 'a70cd2e051dd41c9a3e365ed2d3da003'
+client_secret = 'de50fe22356349619609b5f9830e2387'
+
+client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def getTrackIDs(user, playlist_id):
+    ids = []
+    playlist = sp.user_playlist(user, playlist_id)
+    for item in playlist['tracks']['items']:
+        track = item['track']
+        ids.append(track['id'])
+    return ids
+
+ids = getTrackIDs('Billboard', '6UeSakyzhiEt4NB3UAd6NQ')
+
+def getTrackFeatures(id):
+  meta = sp.track(id)
+  features = sp.audio_features(id)
+
+  # meta
+  name = meta['name']
+  album = meta['album']['name']
+  artist = meta['album']['artists'][0]['name']
+  release_date = meta['album']['release_date']
+  length = meta['duration_ms']
+  popularity = meta['popularity']
+
+  # features
+  acousticness = features[0]['acousticness']
+  danceability = features[0]['danceability']
+  energy = features[0]['energy']
+  instrumentalness = features[0]['instrumentalness']
+  liveness = features[0]['liveness']
+  loudness = features[0]['loudness']
+  speechiness = features[0]['speechiness']
+  tempo = features[0]['tempo']
+  time_signature = features[0]['time_signature']
+
+  track = [name, album, artist, release_date, length, popularity, danceability, acousticness, danceability, energy, instrumentalness, liveness, loudness, speechiness, tempo, time_signature]
+  return track
+
+# loop over track ids 
+tracks = []
+for i in range(len(ids)):
+  time.sleep(.5)
+  track = getTrackFeatures(ids[i])
+  tracks.append(track)
+
+# create dataset
+df = pd.DataFrame(tracks, columns = ['name', 'album', 'artist', 'release_date', 'length', 'popularity', 'danceability', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo', 'time_signature'])
+df.to_csv("spotify.csv", sep = ',')
